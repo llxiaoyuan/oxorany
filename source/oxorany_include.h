66da99b1f32d56d7b7e384a88b287394fd00822a
Chase <36320938+llxiaoyuan@users.noreply.github.com>
2023-03-23 17:34:45 +0800

    Create oxorany_include.h

---
+++ source/oxorany_include.h
@@ -0,0 +1,14 @@
+#ifndef OXORANY_PREDEF_H
+#define OXORANY_PREDEF_H
+
+#define OXORANY_DISABLE_OBFUSCATION
+//use OXORANY_USE_BIT_CAST for remove float double src data
+#define OXORANY_USE_BIT_CAST
+
+#include "oxorany.h"
+
+//to change Marco in a easy way
+#define WRAPPER_MARCO     oxorany
+#define WRAPPER_MARCO_FLT oxorany_flt
+
+#endif
